779debf811d5ea5ea73c62b1bc15d637623442cd
rtchcntlls <your.email@example.com>
2025-06-03 22:36:16 +0800

    edited

--- View/Login.php
+++ View/Login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="../public/img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="Assets/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="Style/Style.css">
     <title>Store Portal</title>
